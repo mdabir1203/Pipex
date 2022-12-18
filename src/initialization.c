f654529fe31391241a418598324f8cbe76d23192
Mohammad abir Abbas <mabbas@c2s13d65.42wolfsburg.de>
2022-12-18 13:30:51 +0100

    pipe initialization and perror setup done

--- src/initialization.c
+++ src/initialization.c
@@ -1,36 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   initialization.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabbas <mabbas@students.42wolfsburg.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/18 02:31:23 by mabbas            #+#    #+#             */
-/*   Updated: 2022/12/18 13:04:37 by mabbas           ###   ########.fr       */
+/*   Created: 2022/12/18 13:05:15 by mabbas            #+#    #+#             */
+/*   Updated: 2022/12/18 13:07:08 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int	pipex(int argc, char **argv, char **envp)
+int	pipe_init(int argc, char **argv, char **envp, t_pipe *pipex)
 {
-	t_pipe	pipex;
-
-	if (argc > 5)
-	{
-		ft_putendl_fd("Argument threshold exceeded!\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	else if (argc == 5)
-	{
-		pipe_init(argc, argv, envp, &pipex);
-		exec_cmd(&pipex);
-		pipe_exit(&pipex);
-	}
-	else
-	{
-		ft_putendl_fd("Argument threshold not covered\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	return (pipe.lock_state);
+	pipe->argc;
+	pipe->argv;
+	pipe->envp;
+	pipe->indx_cmd = argc - 3;
+	pipe->offset = 2;
+	pipe->lock_state = 0;
+	file_open(pipex);
+	return (0);
 }
--- src/main.c
+++ src/main.c
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabbas <mabbas@students.42wolfsburg.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/18 02:31:23 by mabbas            #+#    #+#             */
-/*   Updated: 2022/12/18 13:04:37 by mabbas           ###   ########.fr       */
+/*   Created: 2022/12/18 12:57:19 by mabbas            #+#    #+#             */
+/*   Updated: 2022/12/18 12:59:24 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-int	pipex(int argc, char **argv, char **envp)
+int	main(int argc, char **argv)
 {
-	t_pipe	pipex;
-
-	if (argc > 5)
-	{
-		ft_putendl_fd("Argument threshold exceeded!\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	else if (argc == 5)
-	{
-		pipe_init(argc, argv, envp, &pipex);
-		exec_cmd(&pipex);
-		pipe_exit(&pipex);
-	}
-	else
-	{
-		ft_putendl_fd("Argument threshold not covered\n", 2);
-		exit(EXIT_FAILURE);
-	}
-	return (pipe.lock_state);
+	return (pipex(argc, argv, envp));
 }
