fdf02e70e484f344344b5743c6193a4e9125d317
Mohammad abir Abbas <mabbas@c2s13d65.42wolfsburg.de>
2022-12-19 03:04:44 +0100

    fnc created to search through env and split the cmd

--- src/pipe_utils.c
+++ src/pipe_utils.c
@@ -6,31 +6,74 @@
 /*   By: mabbas <mabbas@students.42wolfsburg.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:13:34 by mabbas            #+#    #+#             */
-/*   Updated: 2022/12/18 14:20:28 by mabbas           ###   ########.fr       */
+/*   Updated: 2022/12/19 03:02:34 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void	dup42(int fd1, int fd2, char **cmd, t_pipe *pipe)
-{
-	if (dup(fd1, fd2) == -1)
-		exec_perror(cmd, "command execution", 1, pipex);
-}
-
-void	replace_all_cmd(char **cmd, char *old_w, char *new_w)
+void	free_ptr(char **ptr)
 {
 	int	i;
 
 	i = 0;
-	while (cmd[i++])
-		replace_all_str(&cmds[i], old_w, new_w);
+	while (ptr[i++])
+		free(ptr[i]);
+	free(ptr);
 }
 
-void	replace_all_str(char **str, char *old_w, char *new_w)
+/**Function to look into the path line inside the environment variable
+ * Each command path is splitted and tested and then returns the correct value.
+**/
+char	*path_find(char *cmd, char **envp)
 {
-	if (!str || !*str)
-		return ;
-	while (ft_strnstr(*str, old_w, ft_strlen(*str)))
-		*str = ft_replace_str(*str, old_w, new_w);
+	char	**path_var;
+	char	*path;
+	int		i;
+	char	*partial_path;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
+	path_var = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (path_var[i])
+	{
+		partial_path = ft_strjoin(path_var[i], "/");
+		path = ft_strjoin(path_var, cmd);
+		free(partial_path);
+		if (get_access(path, 0) == 0)
+			return (path);
+		free (path);
+		i++;
+	}
+	free_path(path_var);
+}
+
+void	error(void)
+{
+	perror("\033[32mError");
+	exit(EXIT_FAILURE);
+}
+
+/**Function to take the cmd and send to find_path before executing.
+ * Here using the execve which transforms the calling process
+ * to a new process.If execve return to calling process then
+ * error will occur and return value is -1.   **/
+void	exec(char *argv, char **envp)
+{
+	char	**cmd;
+	int		i;
+	char	*path;
+
+	i = 0;
+	cmd = ft_split(argv, ' ');
+	path = path_find(cmd[0], envp);
+	if (!path)
+	{
+		free_ptr(cmd);
+		error();
+	}
+	if (execve(path, cmd, envp) == -1)
+		error();
 }
